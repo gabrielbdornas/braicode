0711f63e88e386848f0c3f4ae9d8741409ffdecc
Gabriel Dornas <gabrielbdornas@gmail.com>
2022-07-05 09:01:06 -0300

    Update start server comment

--- build.sh
+++ build.sh
@@ -2,5 +2,5 @@ livemark build blog/index.md --target blog/index.html && \
 livemark build blog/2022-07-04-blog-test.md --target blog/2022-07-04-blog-test.html && \
 livemark build blog/2022-07-04-new-test.md --target blog/2022-07-04-new-test.html && \
 
-# Start server to test builded pages
+# Start server...
 livemark start index.md --target index.html --config livemark.yaml
