e6e8ecc2c7ce3b3a37107febfbfb05d0f86cba12
Gabriel Dornas <gabrielbdornas@gmail.com>
2022-07-04 22:03:58 -0300

    Improve build process with blog pages

--- build.sh
+++ build.sh
@@ -1,2 +1,6 @@
+livemark build blog/index.md --target blog/index.html && \
+livemark build blog/2022-07-04-blog-test.md --target blog/2022-07-04-blog-test.html && \
+livemark build blog/2022-07-04-new-test.md --target blog/2022-07-04-new-test.html && \
+
 # Start server to test builded pages
 livemark start index.md --target index.html --config livemark.yaml
