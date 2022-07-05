13e120958cec2d26ea53b5f30a5027150532696f
Gabriel Dornas <gabrielbdornas@gmail.com>
2022-07-05 12:53:36 -0300

    Remove start from build

--- build.sh
+++ build.sh
@@ -14,6 +14,3 @@ livemark build pages/contrib.md --target pages/contrib.html && \
 livemark build blog/index.md --target blog/index.html && \
 livemark build blog/2022-07-04-blog-test.md --target blog/2022-07-04-blog-test.html && \
 livemark build blog/2022-07-04-new-test.md --target blog/2022-07-04-new-test.html && \
-
-# Start server...
-livemark start index.md --target index.html --config livemark.yaml
